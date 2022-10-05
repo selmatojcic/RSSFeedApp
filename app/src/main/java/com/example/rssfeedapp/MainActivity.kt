d64110a170d41259d6c073743be42ffee78fd089
SelmaTojcic1 <stojcic@etfos.hr>
2022-10-05 17:15:35 +0200

    adding rss feed by link working

--- app/src/main/java/com/example/rssfeedapp/MainActivity.kt
+++ app/src/main/java/com/example/rssfeedapp/MainActivity.kt
@@ -2,30 +2,62 @@ package com.example.rssfeedapp
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.example.rssfeedapp.data.RSSFeedDatabaseBuilder
+import com.example.rssfeedapp.data.RSSFeedRepository
+import com.example.rssfeedapp.databinding.ActivityMainBinding
+import com.example.rssfeedapp.model.Image
 import com.example.rssfeedapp.model.RSS
+import com.example.rssfeedapp.model.RSSFeed
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-    private val api = RSSFeedApi.create()
+    private lateinit var mainBinding: ActivityMainBinding
+    private val rssFeedDao = RSSFeedDatabaseBuilder.getInstance().rssFeedDao()
+    private val rssFeedRepository = RSSFeedRepository(rssFeedDao)
+    private lateinit var editText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
-        val call = api.getRSS()
+        editText = findViewById(R.id.addLinkTextInputEditText)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(
+                    R.id.fragmentContainer,
+                    RSSFeedsFragment.create(),
+                    RSSFeedsFragment.TAG
+                )
+                .commit()
+        }
+    }
+
+    fun requestCall(view: View) {
+        val api = RSSFeedApi.create()
+        val call = api.getRSS(editText.text.toString())
 
         call.enqueue(object : Callback<RSS> {
             override fun onResponse(call: Call<RSS>, response: Response<RSS>) {
-                Log.e("TAG", "Success" + response.body().toString())
-                Log.e("TAG", "Server response$response")
+                response.body()?.channel?.let { addRSSFeed(it.title, it.description, it.image) }
             }
 
             override fun onFailure(call: Call<RSS>, t: Throwable) {
                 Log.e("TAG", "Failure" + t.message)
             }
         })
+    }
+
+    fun addRSSFeed(title: String, description: String, image: Image) {
+        val rssFeed =
+            RSSFeed(id = 0, title = title, description = description, imageURL = image.url)
+        rssFeedRepository.insertFeed(rssFeed)
     }
 }
--- app/src/main/java/com/example/rssfeedapp/RSSFeedApi.kt
+++ app/src/main/java/com/example/rssfeedapp/RSSFeedApi.kt
@@ -5,19 +5,17 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface RSSFeedApi {
 
-    @GET("rss.xml")
-    fun getRSS(): Call<RSS>
+    @GET
+    fun getRSS(@Url url: String): Call<RSS>
 
     companion object {
-        var BASE_URL = "https://feeds.bbci.co.uk/news/world/"
-
         fun create(): RSSFeedApi {
-
             val retrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL)
+                .baseUrl("https://feeds.bbci.co.uk/news/world/")
                 .addConverterFactory(SimpleXmlConverterFactory.create())
                 .build()
             return retrofit.create(RSSFeedApi::class.java)
